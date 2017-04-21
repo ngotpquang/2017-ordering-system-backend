0bdec033a5d1614860d3afc1f00d3b22cc18a013
Quang Ngo <ngotruongphamquang@gmail.com>
2017-04-21 09:45:10 +0700

    Edit delete food and drink type to invisible all food and drink of that type

--- src/main/java/com/alfrescos/orderingsystem/controller/FoodAndDrinkTypeController.java
+++ src/main/java/com/alfrescos/orderingsystem/controller/FoodAndDrinkTypeController.java
@@ -4,13 +4,19 @@
 
 package com.alfrescos.orderingsystem.controller;
 
+import com.alfrescos.orderingsystem.entity.FoodAndDrink;
 import com.alfrescos.orderingsystem.entity.FoodAndDrinkType;
+import com.alfrescos.orderingsystem.entity.OrderCombination;
+import com.alfrescos.orderingsystem.service.FoodAndDrinkService;
 import com.alfrescos.orderingsystem.service.FoodAndDrinkTypeService;
+import com.alfrescos.orderingsystem.service.OrderCombinationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by Liger on 16-Mar-17.
@@ -21,6 +27,12 @@ public class FoodAndDrinkTypeController {
 
     @Autowired
     private FoodAndDrinkTypeService foodAndDrinkTypeService;
+
+    @Autowired
+    private FoodAndDrinkService foodAndDrinkService;
+
+    @Autowired
+    private OrderCombinationService orderCombinationService;
 
     @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
     @PostMapping
@@ -55,6 +67,22 @@ public class FoodAndDrinkTypeController {
     @DeleteMapping(value = "/delete/{foodAndDrinkTypeId}")
     public ResponseEntity<?> delete(@PathVariable Long foodAndDrinkTypeId){
         FoodAndDrinkType foodAndDrinkType = this.foodAndDrinkTypeService.findById(foodAndDrinkTypeId);
+        List<FoodAndDrink> foodAndDrinkList = this.foodAndDrinkService.findByFoodAndDrinkTypeId(foodAndDrinkTypeId);
+        for (FoodAndDrink foodAndDrink: foodAndDrinkList) {
+            List<OrderCombination> orderCombinationList;
+            foodAndDrink.setVisible(!foodAndDrink.isVisible());
+            this.foodAndDrinkService.update(foodAndDrink);
+            if (foodAndDrinkType.isMainDish()) {
+                orderCombinationList = this.orderCombinationService.findByMainDishId(foodAndDrink.getId());
+            } else {
+                orderCombinationList = this.orderCombinationService.findByDrinkOrDesertId(foodAndDrink.getId());
+            }
+            for (OrderCombination o : orderCombinationList) {
+                boolean isVisible = o.getMainDish().isVisible() && o.getDrinkOrDesert().isVisible();
+                o.setVisible(isVisible);
+                this.orderCombinationService.updateVisible(o);
+            }
+        }
         if (foodAndDrinkType != null){
             return new ResponseEntity<Object>("Visible status of FADT with id: " + foodAndDrinkTypeId + " is: " + this.foodAndDrinkTypeService.switchVisible(foodAndDrinkTypeId), HttpStatus.OK);
         }
