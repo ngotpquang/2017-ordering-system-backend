c2384e07c62fcd4d2ee40bb0353b47273a208b9d
Quang Ngo <ngotruongphamquang@gmail.com>
2017-04-23 21:33:50 +0700

    Add function to return location

--- src/main/java/com/alfrescos/orderingsystem/controller/HomeController.java
+++ src/main/java/com/alfrescos/orderingsystem/controller/HomeController.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2017. All rights reserved.
+ */
+
 package com.alfrescos.orderingsystem.controller;
 
 import org.springframework.http.HttpStatus;
@@ -18,5 +22,10 @@ public class HomeController {
     @GetMapping
     public ResponseEntity<String> testGet() {
         return new ResponseEntity<>("Ordering System Web Service is running. Time now is: " + new Date(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/location")
+    public ResponseEntity<?> getLocation(){
+        return new ResponseEntity<Object>("16.0653281,108.2212667", HttpStatus.OK);
     }
 }
