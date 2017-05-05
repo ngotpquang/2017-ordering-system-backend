271a34115d7bafcfed745094cc29d069b7fb0b0f
Quang Ngo <ngotruongphamquang@gmail.com>
2017-05-05 19:55:30 +0700

    Add status number for reserved table

--- src/main/java/com/alfrescos/orderingsystem/common/TableStatus.java
+++ src/main/java/com/alfrescos/orderingsystem/common/TableStatus.java
@@ -12,5 +12,6 @@ public class TableStatus {
     public static final int ORDERING = 1;
     public static final int FOOD_IS_MADE = 2;
     public static final int CLEANING = 3;
+    public static final int RESERVED = 4;
 
 }
